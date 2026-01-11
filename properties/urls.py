bd8ee0fae5414d8c3ff79c577875aab8e0fc799d
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-11 22:51:48 +0000

    Update urls.py

--- properties/urls.py
+++ properties/urls.py
@@ -1,1 +1,17 @@
+from django.core.cache import cache
+from .models import Property
+
+def getallproperties():
+    # Try to get from cache
+    queryset = cache.get('allproperties')
+    if queryset is not None:
+        return queryset
+
+    # Fetch from database
+    queryset = list(Property.objects.all())
+
+    # Store in cache for 1 hour
+    cache.set('allproperties', queryset, 3600)
+
+    return queryset
 
