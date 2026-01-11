5ed2fa7fa9b9e0f02a3dbd9409691db6152e56aa
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-11 22:48:40 +0000

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -1,1 +1,19 @@
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
+from .utils import get_all_properties
 
+@cache_page(60 * 15)  # cache 15 minutes
+def property_list(request):
+    properties = get_all_properties()
+    data = [
+        {
+            "id": prop.id,
+            "title": prop.title,
+            "description": prop.description,
+            "price": float(prop.price),
+            "location": prop.location,
+            "created_at": prop.created_at.isoformat(),
+        }
+        for prop in properties
+    ]
+    return JsonResponse({"properties": data})
