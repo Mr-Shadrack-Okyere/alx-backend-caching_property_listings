e580ecae767eadd6dda8d9caa44d2423112f1525
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-11 22:52:17 +0000

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -1,10 +1,10 @@
 from django.views.decorators.cache import cache_page
 from django.http import JsonResponse
-from .utils import get_all_properties
+from .utils import getallproperties  # <- updated import
 
 @cache_page(60 * 15)  # cache 15 minutes
 def property_list(request):
-    properties = get_all_properties()
+    properties = getallproperties()  # <- call updated function
     data = [
         {
             "id": prop.id,
